e65d80fb00dd2853232d5268f33721aa1e705922
woojung8405 <woojung8405@gmail.com>
2023-07-22 09:54:57 +0900

    feature Health Check API

---
+++ src/main/java/com/app/api/health/controller/HealthCheckController.java
@@ -0,0 +1,31 @@
+package com.app.api.health.controller;
+
+import com.app.api.health.dto.HealthCheckResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class HealthCheckController {
+
+    private final Environment environment;
+
+    @GetMapping("/health")
+    public ResponseEntity<HealthCheckResponseDto> healthCheck() {
+        HealthCheckResponseDto healthCheckResponseDto = HealthCheckResponseDto.builder()
+                .health("ok")
+                .activeProfiles(Arrays.asList(environment.getActiveProfiles()))
+                .build();
+
+        return ResponseEntity.ok(healthCheckResponseDto);
+    }
+
+
+}
